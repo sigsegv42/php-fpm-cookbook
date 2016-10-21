d0c0d1f8debde09746071c2a3453697319471b49
Adrian Almenar <adrian@tecnocratica.net>
2016-10-21 12:16:58 +0200

    Bump version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Installs/Configures php-fpm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 issues_url       'https://github.com/tecnocratica/php-fpm-cookbook/issues'
 source_url       'https://github.com/tecnocratica/php-fpm-cookbook'
 
