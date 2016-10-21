b917eb21ea75e836f161f169b96a79bce3835c84
Adrian Almenar <adrian@tecnocratica.net>
2016-10-21 11:36:53 +0200

    Fixing foodcritic issues

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,16 @@
-name 'php-fpm'
+name             'php-fpm'
 maintainer       'Opscode, Inc.'
 maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Installs/Configures php-fpm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+issues_url       'https://github.com/tecnocratica/php-fpm-cookbook/issues'
+source_url       'https://github.com/tecnocratica/php-fpm-cookbook'
 
 depends 'apt'
 depends 'yum', '>= 3.0'
 
-%w{ debian ubuntu centos redhat fedora amazon }.each do |os|
+%w{ debian ubuntu centos redhat fedora }.each do |os|
   supports os
 end
