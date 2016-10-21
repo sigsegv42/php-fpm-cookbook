abfedbbca915cb8124bb005428c1bdabc7eecec0
Adrian Almenar <adrian@tecnocratica.net>
2016-10-21 11:18:48 +0200

    Change version to 1.0, put everything single quoted

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name "php-fpm"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Installs/Configures php-fpm"
+name 'php-fpm'
+maintainer       'Opscode, Inc.'
+maintainer_email 'cookbooks@opscode.com'
+license          'Apache 2.0'
+description      'Installs/Configures php-fpm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.7.6"
+version          '1.0.0'
 
-depends "apt"
-depends "yum", ">= 3.0"
+depends 'apt'
+depends 'yum', '>= 3.0'
 
 %w{ debian ubuntu centos redhat fedora amazon }.each do |os|
   supports os
