82afc8ed9d06acc99fd81ffc5e507754df86f467
Adrian Almenar <adrian@tecnocratica.net>
2016-10-21 12:54:06 +0200

    Merge patch from https://github.com/yevgenko/cookbook-php-fpm/pull/90

--- test/integration/default/serverspec/pools_spec.rb
+++ test/integration/default/serverspec/pools_spec.rb
@@ -5,3 +5,7 @@ set :backend, :exec
 describe port(9081) do
   it { should be_listening }
 end
+
+describe file('/etc/php-fpm.d/web.conf') do
+  its(:content) { should match /pm.status_path = \/status/ }
+end
